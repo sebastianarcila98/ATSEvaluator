3449a8977258432472caaaef468ceed2e985d091
Sebastian Arcila <sebastian.arcila@myfuelmaster.com>
2022-12-09 10:19:22 -0500

    initial push

---
+++ src/main/java/com/atsevaluator/atsevaluator/AtsevaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.atsevaluator.atsevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsevaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsevaluatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsevaluator/atsevaluator/AtsevaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsevaluator.atsevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsevaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
